de15c56ee6e08a2c508e9870c9b5fa9a20c2136c
parekhd <danielparekh@protonmail.com>
2020-07-24 15:02:10 +0100

    Enable full range of magnitude for shift cypher

--- src/decryptme/CipherGenerator.java
+++ src/decryptme/CipherGenerator.java
@@ -1,18 +1,23 @@
 package decryptme;
 
+import java.util.Random;
+
 public class CipherGenerator {
 
-	public static int SHIFT_AMOUNT = 3;
+	public static Random random = new Random();
+	public static int SHIFT_MIN = 1;
+	public static int SHIFT_MAX = 25;
 	
 	public static String shift(String plaintext) {
 		int len = plaintext.length();
+		int shift = random.nextInt(SHIFT_MAX - SHIFT_MIN) + SHIFT_MIN;
 		
 		StringBuilder sb = new StringBuilder();
 		
 		for (int i = 0; i < len; i++) {
-			char c = (char)(plaintext.charAt(i) + SHIFT_AMOUNT);
+			char c = (char)(plaintext.charAt(i) + shift);
 			if(c > 'z') {
-				sb.append((char)(plaintext.charAt(i)  - (26-SHIFT_AMOUNT)));
+				sb.append((char)(plaintext.charAt(i)  - (26 - shift)));
 			} else {
 				sb.append(c);
 			}
